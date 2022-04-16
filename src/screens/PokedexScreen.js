296fdd5b10f4d01efc2c700a899d18b234cc9306
CristoCG <cristobalcabrerag@hotmail.com>
2022-04-16 16:18:28 -0500

    Fetch del api de pokemon

--- src/screens/PokedexScreen.js
+++ src/screens/PokedexScreen.js
@@ -1,7 +1,23 @@
+import React, { useEffect, useState } from "react";
 import { Text, SafeAreaView } from "react-native";
-import React from "react";
+import { getPokemonsApi } from "../api/pokemon";
 
 export default function PokedexScreen() {
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
