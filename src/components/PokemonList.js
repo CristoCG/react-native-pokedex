fefab011eed1d2aca26ca0a61b3efccdbb8c22a9
CristoCG <cristobalcabrerag@hotmail.com>
2022-04-17 14:58:12 -0500

    Añadido el spinner y el infinity scroll

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { FlatList, Text, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, ActivityIndicator } from "react-native";
+
 import PokemonCard from "./PokemonCard";
 
 export default function PokemonList(props) {
   const { pokemons } = props;
+
+  //Función para hacer infinity scroll
+  const loadMore = () => {
+    console.log("Cargando más pokemons!...");
+  };
   return (
     <>
       <FlatList
@@ -13,6 +19,14 @@ export default function PokemonList(props) {
         keyExtractor={(pokemon) => String(pokemon.id)}
         renderItem={({ item }) => <PokemonCard pokemon={item} />}
         contentContainerStyle={styles.flatListContentContainer}
+        //Empieza el infinity scroll
+        onEndReached={loadMore}
+        //Empieza a cargar antes de que se llegue al final
+        onEndReachedThreshold={0.1}
+        //Spinner para cuando esté cargando
+        ListFooterComponent={
+          <ActivityIndicator size="large" style={styles.spinner} />
+        }
       />
     </>
   );
@@ -22,5 +36,9 @@ export default function PokemonList(props) {
 const styles = StyleSheet.create({
   flatListContentContainer: {
     paddingHorizontal: 5,
+  },
+  spinner: {
+    marginTop: 20,
+    marginBottom: 60,
   },
 });
