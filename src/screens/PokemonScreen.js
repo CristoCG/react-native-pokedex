3a94fe96f8968c6c2f005b3affc9ef37013554d1
CristoCG <cristobalcabrerag@hotmail.com>
2022-04-18 17:45:49 -0500

    Cambiabndo el icono de volver atrás funcional

--- src/screens/PokemonScreen.js
+++ src/screens/PokemonScreen.js
@@ -1,15 +1,46 @@
 import { ScrollView } from "react-native";
 import React, { useState, useEffect } from "react";
+
 import { getUniquePokemonDetailApi } from "../api/pokemon";
 
 import Header from "../components/Pokemon/Header";
 import Type from "../components/Pokemon/Type";
 import Stats from "../components/Pokemon/Stats";
 
+import Icon from "react-native-vector-icons/FontAwesome5";
+
 export default function PokemonScreen(props) {
   const { navigation, route } = props;
 
   const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    navigation.setOptions({
+      //icono de favoritos iria aquí
+      headerRight: () => (
+        <Icon
+          name="heart"
+          color="#fff"
+          size={20}
+          style={{ marginLeft: 20 }}
+          onPress={() => {
+            console.log("Añadirlo a favoritos");
+          }}
+        />
+      ),
+
+      //Cambiar el icono para volver atrás
+      headerLeft: () => (
+        <Icon
+          name="arrow-left"
+          color="#fff"
+          size={20}
+          style={{ marginLeft: 20 }}
+          onPress={navigation.goBack}
+        />
+      ),
+    });
+  }, [navigation, route.params]);
 
   useEffect(() => {
     //Función anónima autoejecutable
